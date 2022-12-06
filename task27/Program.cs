bafc6aad66a250ea7656fba4cde48e86f2391f4c
Ilya <silvercrow735@gmail.com>
2022-12-06 22:55:21 +0300

    task27

---
+++ task27/Program.cs
@@ -0,0 +1,12 @@
+﻿int line = int.Parse(Console.ReadLine());
+Console.WriteLine($"{line}->{func(line)}");
+
+int func(int numbers){
+    int summ = 0;
+    while (numbers > 0)
+    {
+        summ = summ + numbers % 10;
+        numbers /= 10;
+    }
+    return summ;
+}
